28b77a2909ad3b4529b324a411ca5e88a1a00de1
BurkeStrang <burke.strang@outlook.com>
2024-03-03 16:14:09 -0500

    more

---
+++ blind75.Tests/AdvancedGraphs/AlienDictionary.Tests.cs
@@ -0,0 +1,21 @@
+// Input：["wrt","wrf","er","ett","rftt"]
+// Output："wertf"
+// Explanation：
+// from "wrt"and"wrf" ,we can get 't'<'f'
+// from "wrt"and"er" ,we can get 'w'<'e'
+// from "er"and"ett" ,we can get 'r'<'t'
+// from "ett"and"rftt" ,we can get 'e'<'r'
+// So return "wertf"
+
+
+public class AlienDictionaryTest
+{
+    [Fact]
+    public void AlienDictionaryCorrect()
+    {
+        string[] input = new string[] { "wrt", "wrf", "er", "ett", "rftt" };
+        string exp = "wertf";
+
+        Assert.Equal(exp, AlienDictionaryClass.AlienOrder(input));
+    }
+}
---
+++ blind75/AdvancedGraphs/AlienDictionary.cs
@@ -0,0 +1,93 @@
+
+// There is a new alien language which uses the latin alphabet.
+// However, the order among letters are unknown to you.
+// You receive a list of non-empty words from the dictionary,
+// where words are sorted lexicographically by the rules of this new language.
+// Derive the order of letters in this language.
+
+// Input：["wrt","wrf","er","ett","rftt"]
+// Output："wertf"
+// Explanation：
+// from "wrt"and"wrf" ,we can get 't'<'f'
+// from "wrt"and"er" ,we can get 'w'<'e'
+// from "er"and"ett" ,we can get 'r'<'t'
+// from "ett"and"rftt" ,we can get 'e'<'r'
+// So return "wertf"
+
+public static class AlienDictionaryClass
+{
+    public static string AlienOrder(string[] words)
+    {
+
+        var adj = new Dictionary<char, HashSet<char>>();
+
+        //Add all the available characters to the adjacency list resolves all the edges 
+        foreach (var word in words)
+        {
+            foreach (var c in word)
+            {
+                if (adj.ContainsKey(c)) continue;
+                adj[c] = new HashSet<char>();
+                //alphabet.Add(c);
+            }
+        }
+
+        for (var i = 0; i < words.Length - 1; i++)
+        {
+            var w1 = words[i];
+            var w2 = words[i + 1];
+
+            var minLen = Math.Min(w1.Length, w2.Length);
+            if (w1.Length > w2.Length && w1.Substring(0, minLen) == w2.Substring(0, minLen))
+            {
+                return "";
+            }
+
+            for (var j = 0; j < minLen; j++)
+            {
+                if (w1[j] != w2[j])
+                {
+                    adj[w1[j]].Add(w2[j]);
+                    break;
+                }
+            }
+
+        }
+
+        var visited = new Dictionary<char, bool>(); //false = visited, true = in the current path
+        var res = new List<char>();
+
+        bool dfs(char c)
+        {
+            if (visited.ContainsKey(c))
+                return visited[c]; //true: there is a cycle - we are visiting this twice
+
+
+            visited.TryAdd(c, false);
+            visited[c] = true;
+
+            foreach (var neigh in adj[c])
+            {
+
+                if (dfs(neigh))
+                    return true;
+            }
+
+
+            visited[c] = false;
+            res.Add(c);
+
+            return visited[c];
+        }
+
+        foreach (var c in adj.Keys)
+        {
+            if (dfs(c))
+                return "";
+        }
+
+        res.Reverse();
+        return string.Join("", res);
+
+    }
+}
