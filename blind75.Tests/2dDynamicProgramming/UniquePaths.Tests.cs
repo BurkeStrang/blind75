33c85effbd9fbc1536470affda1ce7328c6ce672
BurkeStrang <burke.strang@outlook.com>
2024-03-08 16:09:02 -0500

    batman

---
+++ blind75.Tests/2dDynamicProgramming/UniquePaths.Tests.cs
@@ -0,0 +1,20 @@
+public class UniquePathsTest
+{
+    [Fact]
+    public void CorrectUniquePaths()
+    {
+        int m = 3;
+        int n = 2;
+        int expected = 3;
+        Assert.Equal(expected, UniquePathsClass.UniquePaths(m, n));
+    }
+
+    [Fact]
+    public void CorrectUniquePaths2()
+    {
+        int m = 7;
+        int n = 3;
+        int expected = 28;
+        Assert.Equal(expected, UniquePathsClass.UniquePaths(m, n));
+    }
+}
---
+++ blind75/2dDynamicProgramming/UniquePaths.cs
@@ -0,0 +1,43 @@
+namespace blind75.DynammicProgramming;
+
+/*
+
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]).
+The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+The robot can only move either down or right at any point in time.
+How many possible unique paths are there?
+
+Given the two integers m and n,
+return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+Example:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+  */
+
+public static class UniquePathsClass
+{
+    public static int UniquePaths(int m, int n)
+    {
+        int[] row = new int[n];
+        Array.Fill(row, 1);
+        foreach (int i in Enumerable.Range(0, m - 1))
+        {
+            int[] newRow = new int[n];
+            Array.Fill(newRow, 1);
+            for (int j = n - 2; j >= 0; j--)
+            {
+                newRow[j] = newRow[j + 1] + row[j];
+            }
+            row = newRow;
+        }
+        return row[0];
+    }
+}
