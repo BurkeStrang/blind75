618736a2f601ac78b2069f73dd222c498ab846fb
Burke.Strang <burke.strang@vertigis.com>
2024-03-11 16:30:30 -0400

    batman

--- blind75/Graphs/CourseSchedule.cs
+++ blind75/Graphs/CourseSchedule.cs
@@ -1,4 +1,5 @@
 namespace blind75.Graphs;
+
 // There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
 // You are given an array prerequisites where prerequisites[i] = [ai, bi]
 // indicates that you must take course bi first if you want to take course ai.
@@ -10,16 +11,22 @@ namespace blind75.Graphs;
 
 // Input: numCourses = 2, prerequisites = [[1,0]]
 // Output: true
-// Explanation: There are a total of 2 courses to take. 
+// Explanation: There are a total of 2 courses to take.
 // To take course 1 you should have finished course 0. So it is possible.
+
+// Example 2:
+// Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+// Output: false
+// Explanation: There are a total of 2 courses to take.
+// To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1.
+// So it is impossible.
 
 public static class CourseScheduleClass
 {
     public static bool CanFinish(int numCourses, int[][] prerequisites)
     {
-
-        IDictionary<int, List<int>> preMap = new Dictionary<int, List<int>>();
-        HashSet<int> visited = new HashSet<int>();
+        Dictionary<int, List<int>> preMap = new();
+        HashSet<int> visited = new();
         for (int i = 0; i < numCourses; i++)
         {
             preMap.Add(i, new List<int>());
@@ -41,6 +48,7 @@ public static class CourseScheduleClass
         }
         return true;
     }
+
     public static bool DfsGraph(IDictionary<int, List<int>> preMap, HashSet<int> visited, int crs)
     {
         if (visited.Contains(crs))
@@ -52,7 +60,7 @@ public static class CourseScheduleClass
             return true;
         }
         visited.Add(crs);
-        foreach (var pre in preMap[crs])
+        foreach (int pre in preMap[crs])
         {
             if (!DfsGraph(preMap, visited, pre))
             {
@@ -60,7 +68,7 @@ public static class CourseScheduleClass
             }
         }
         visited.Remove(crs);
-        preMap[crs] = new List<int>();
+        preMap[crs] = new();
         return true;
     }
 }
--- blind75/Intervals/MeetingRooms.cs
+++ blind75/Intervals/MeetingRooms.cs
@@ -1,6 +1,10 @@
-/* Given an array of meeting time intervals consisting of start and end times
+/*
+
+Given an array of meeting time intervals consisting of start and end times
 [[s1,e1],[s2,e2],...] (si < ei),
-determine if a person could attend all meetings. */
+determine if a person could attend all meetings.
+
+*/
 
 namespace blind75.Intervals;
 
@@ -11,6 +15,8 @@ public static class MeetingRooms
         Array.Sort(intervals, (a, b) => a[0] - b[0]);
         for (int i = 1; i < intervals.Length; i++)
         {
+            // if the start time of the next meeting
+            // is less than the end time of the previous meeting
             if (intervals[i][0] < intervals[i - 1][1])
                 return false;
         }
