4c51eb0c3fc7f8ff0779ccc9ed522f4db7e46506
Burke Strang <Burke.Strang@vertigis.com>
2024-04-25 06:53:41 -0400

    adding test

--- blind75.Tests/General/FizzBuzz.Tests.cs
+++ blind75.Tests/General/FizzBuzz.Tests.cs
@@ -10,6 +10,19 @@ public class FizzBuzzTests
         // Act
         string output = FizzBuzz.PrintFizzBuzz(n);
         // Assert
-        Assert.Equal("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n", output.ToString());
+        if (Environment.NewLine == "\r\n")
+        {
+            Assert.Equal(
+                "1\r\n2\r\nFizz\r\n4\r\nBuzz\r\nFizz\r\n7\r\n8\r\nFizz\r\nBuzz\r\n11\r\nFizz\r\n13\r\n14\r\nFizzBuzz\r\n",
+                output
+            );
+        }
+        else
+        {
+            Assert.Equal(
+                "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n",
+                output
+            );
+        }
     }
 }
--- blind75/1dDynamicProgramming/LongestIncreasingSubsequence.cs
+++ blind75/1dDynamicProgramming/LongestIncreasingSubsequence.cs
@@ -16,8 +16,18 @@ public static class LongestIncreasingSubsequence
         int[] dp = new int[nums.Length];
         Array.Fill(dp, 1);
 
+        // go through the array from right to left
+        // and compare the current element with the next elements
+        // if the current element is less than the next element
+        // then the current element is part of the increasing subsequence
+        // and the length of the increasing subsequence is 1 + the length of the next element
+        // if the current element is greater than the next element
+        // then the current element is not part of the increasing subsequence
+        // and the length of the increasing subsequence is the length of the next element
+        // keep track of the maximum length of the increasing subsequence
         for (int i = nums.Length - 1; i >= 0; i--)
         {
+            Console.WriteLine($"i: {i}");
             for (int j = i + 1; j < nums.Length; j++)
             {
                 if (nums[i] < nums[j])
