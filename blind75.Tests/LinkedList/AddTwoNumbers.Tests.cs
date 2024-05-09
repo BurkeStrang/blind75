d9efcf953740281f6d55c8adf55ef5664a86c276
Burke.Strang <burke.strang@vertigis.com>
2024-05-09 13:44:13 -0400

    addtwonumbers

---
+++ blind75.Tests/LinkedList/AddTwoNumbers.Tests.cs
@@ -0,0 +1,47 @@
+namespace Blind75.LinkedList;
+
+public class AddTwoNumbersTests
+{
+    [Fact]
+    public void Example1()
+    {
+        // Arrange
+        ListNode l1 = new(2)
+        {
+            next = new(4)
+            {
+                next = new(3)
+            }
+        };
+        ListNode l2 = new(5)
+        {
+            next = new(6)
+            {
+                next = new(4)
+            }
+        };
+        ListNode expected = new(7)
+        {
+            next = new(0)
+            {
+                next = new(8)
+            }
+        };
+        // Act
+        ListNode? result = AddTwoNumbersClass.AddTwoNumbers(l1, l2);
+        // Assert
+        Assert.True(expected.AreEqual(result));
+    }
+    [Fact]
+    public void Example2()
+    {
+        // Arrange
+        ListNode l1 = new(0);
+        ListNode l2 = new(0);
+        ListNode expected = new(0);
+        // Act
+        ListNode? result = AddTwoNumbersClass.AddTwoNumbers(l1, l2);
+        // Assert
+        Assert.True(expected.AreEqual(result));
+    }
+}
