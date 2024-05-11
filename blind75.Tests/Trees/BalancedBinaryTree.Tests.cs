705ab9f4a4d81a64373465b8dbdc3200a43a10f5
BurkeStrang <burke.strang@outlook.com>
2024-05-11 17:07:02 -0400

    new trees

---
+++ blind75.Tests/Trees/BalancedBinaryTree.Tests.cs
@@ -0,0 +1,35 @@
+namespace Blind75.Trees;
+
+public class BalancedBinaryTreeTests
+{
+    [Fact]
+    public void IsBalanced_GivenExample1_ReturnsTrue()
+    {
+        // Arrange
+        TreeNode root = new(3, new(9), new(20, new(15), new(7)));
+        // Act
+        bool result = new BalancedBinaryTreeClass().IsBalanced(root);
+        // Assert
+        Assert.True(result);
+    }
+    [Fact]
+    public void IsBalanced_GivenExample2_ReturnsFalse()
+    {
+        // Arrange
+        TreeNode root = new(1, new(2, new(3, new(4), new(4)), new(3)));
+        // Act
+        bool result = new BalancedBinaryTreeClass().IsBalanced(root);
+        // Assert
+        Assert.False(result);
+    }
+    [Fact]
+    public void IsBalanced_GivenExample3_ReturnsTrue()
+    {
+        // Arrange
+        TreeNode? root = null;
+        // Act
+        bool result = new BalancedBinaryTreeClass().IsBalanced(root);
+        // Assert
+        Assert.True(result);
+    }
+}
---
+++ blind75.Tests/Trees/DiameterOfBinaryTree.Tests.cs
@@ -0,0 +1,26 @@
+namespace Blind75.Trees;
+
+public class DiameterOfBinaryTreeTests
+{
+    [Fact]
+    public void DiameterOfBinaryTree_GivenExample1_Returns3()
+    {
+        // Arrange
+        TreeNode root = new(1, new(2, new(4), new(5)), new(3));
+        // Act
+        int result = DiameterOfBinaryTreeClass.DiameterOfBinaryTree(root);
+        // Assert
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void DiameterOfBinaryTree_GivenExample2_Returns1()
+    {
+        // Arrange
+        TreeNode root = new(1, new(2));
+        // Act
+        int result = DiameterOfBinaryTreeClass.DiameterOfBinaryTree(root);
+        // Assert
+        Assert.Equal(1, result);
+    }
+}
