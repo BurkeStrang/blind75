f300ffdfa08f17232eba110beb5897eae26096d2
Burke.Strang <burke.strang@vertigis.com>
2024-05-14 09:56:23 -0400

    formatting

--- blind75.Tests/Greedy/JumpGame.Tests.cs
+++ blind75.Tests/Greedy/JumpGame.Tests.cs
@@ -1,10 +1,12 @@
+namespace Blind75.Greedy;
+
 public class JumpGameTests
 {
     [Fact]
     public void CanJumpWhenCalledReturnsTrueIfCanReachLastIndex()
     {
         // Arrange
-        int[] nums = { 2, 3, 1, 1, 4 };
+        int[] nums = [2, 3, 1, 1, 4];
         // Act
         bool result = JumpGame.CanJump(nums);
         // Assert
@@ -15,7 +17,7 @@ public class JumpGameTests
     public void CanJumpWhenCalledReturnsFalseIfCannotReachLastIndex()
     {
         // Arrange
-        int[] nums = { 3, 2, 1, 0, 4 };
+        int[] nums = [3, 2, 1, 0, 4];
         // Act
         bool result = JumpGame.CanJump(nums);
         // Assert
