6e573559d86b03fc2e5335b041c5828231eea329
BurkeStrang <burke.strang@outlook.com>
2024-05-19 10:41:19 -0400

    fix test

--- blind75.Tests/AdvancedGraphs/CheapestFlightWithinKStops.Tests.cs
+++ blind75.Tests/AdvancedGraphs/CheapestFlightWithinKStops.Tests.cs
@@ -47,22 +47,19 @@ public class CheapestFlightWithinKStopsTests
     public void Example3()
     {
         // Arrange
-        int n = 5;
+        int n = 3;
         int[][] flights =
         [
-            [0, 1, 5],
-            [1, 2, 5],
-            [0, 3, 2],
-            [3, 1, 2],
-            [1, 4, 1],
-            [4, 2, 1]
+            [0, 1, 100],
+            [1, 2, 100],
+            [0, 2, 500]
         ];
         int src = 0;
         int dst = 2;
-        int k = 2;
+        int k = 0;
         // Act
         int result = CheapestFlightsWithinKStops.FindCheapestPrice(n, flights, src, dst, k);
         // Assert
-        Assert.Equal(7, result);
+        Assert.Equal(500, result);
     }
 }
