2028187d89aba63e2c9a77a5597debd88bfe08e5
Burke.Strang <burke.strang@vertigis.com>
2024-06-10 16:50:21 -0400

    practice

--- blind75.Tests/ArraysAndHashing/ValidAnagrams.Tests.cs
+++ blind75.Tests/ArraysAndHashing/ValidAnagrams.Tests.cs
@@ -1,3 +1,5 @@
+namespace Blind75.ArraysAndHashing;
+
 public class ValidAnagramsTests
 {
     [Fact]
--- blind75/ArraysAndHashing/ValidAnagram.cs
+++ blind75/ArraysAndHashing/ValidAnagram.cs
@@ -18,18 +18,15 @@ public static class ValidAnagramClass
     {
         if (s.Length != t.Length)
             return false;
-        int[] count = new int[26];
+
+        int[] charChecker = new int[26];
+
         for (int i = 0; i < s.Length; i++)
         {
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
+            charChecker[s[i] - 'a']++;
+            charChecker[t[i] - 'a']--;
         }
-        return count.All(c => c == 0);
-        /*  for (int i = 0; i < 26; i++)
-         {
-             if (!(count[i] == 0))
-                 return false;
-         }
-         return true; */
+
+        return charChecker.All(c => c == 0);
     }
 }
