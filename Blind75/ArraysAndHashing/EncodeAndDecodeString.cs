895e419bf36a2641eb2fd097135400b4ce68aa67
Burke Strang <Burke.Strang@vertigis.com>
2024-08-02 16:21:45 -0400

    new test

--- Blind75/ArraysAndHashing/EncodeAndDecodeString.cs
+++ Blind75/ArraysAndHashing/EncodeAndDecodeString.cs
@@ -17,10 +17,11 @@ public static class Codec
     {
         int i = 0;
         IList<string> res = [];
-        while(i < s.Length)
+        while (i < s.Length)
         {
+            // skips the next character so could be anything
             int length = s[i++] - '0';
-            res.Add(s.Substring(++i,length));
+            res.Add(s.Substring(++i, length));
             i += length;
         }
         return res;
