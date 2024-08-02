9a5f31c707f522b38f377b977572a15172d09739
BurkeStrang <burke.strang@outlook.com>
2024-08-02 10:09:42 -0400

    chore: adding more rebase squash
    remove fake

--- Blind75/ArraysAndHashing/ContainsDuplicate.cs
+++ Blind75/ArraysAndHashing/ContainsDuplicate.cs
@@ -12,6 +12,6 @@ public static class ContainsDuplicateClass
     public static bool ContainsDuplicate(int[] nums)
     {
         HashSet<int> set = [];
-        return nums.Any(n => !set.Add(n));
+        return nums.All(set.Add);
     }
 }
