3f2ac6df276a367319d1757e5a1cda75bb98f9af
BurkeStrang <burke.strang@outlook.com>
2024-08-19 14:39:17 -0400

    two pointer

---
+++ Blind75.Tests/TwoPointers/MinDiffBetHighLowKScore.Tests.cs
@@ -0,0 +1,30 @@
+namespace Blind75.Tests.TwoPointers;
+
+public class MinDiffBetHighLowKScoreTests
+{
+    [Fact]
+    public void Example1()
+    {
+        // Arrange
+        int[] nums = [90];
+        int k = 1;
+        int expected = 0;
+        // Act
+        int actual = MinDiffBetHighLowKScore.MinDifference(nums, k);
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        // Arrange
+        int[] nums = [9, 4, 1, 7];
+        int k = 2;
+        int expected = 2;
+        // Act
+        int actual = MinDiffBetHighLowKScore.MinDifference(nums, k);
+        // Assert
+        actual.Should().Be(expected);
+    }
+}
