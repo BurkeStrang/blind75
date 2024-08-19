42a5788012cdd0744a329d8f9db23f20f7b7dab8
BurkeStrang <burke.strang@outlook.com>
2024-08-19 07:55:20 -0400

    First Unique Char

---
+++ Blind75.Tests/ArraysAndHashing/FirstUniqueCharInString.Tests.cs
@@ -0,0 +1,40 @@
+namespace Blind75.Tests.ArraysAndHashing;
+
+public class FirstUniqueCharInStringTests
+{
+    [Fact]
+    public void Example1()
+    {
+        // Arrange
+        string s = "leetcode";
+        int expected = 0;
+        // Act
+        int result = FirstUniqueCharInString.FirstUniqChar(s);
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        // Arrange
+        string s = "loveleetcode";
+        int expected = 2;
+        // Act
+        int result = FirstUniqueCharInString.FirstUniqChar(s);
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Example3()
+    {
+        // Arrange
+        string s = "aabb";
+        int expected = -1;
+        // Act
+        int result = FirstUniqueCharInString.FirstUniqChar(s);
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ Blind75/ArraysAndHashing/FirstUniqueCharInString.cs
@@ -0,0 +1,39 @@
+namespace Blind75.ArraysAndHashing;
+
+// Given a string s, find the first non-repeating character in it and return its index.
+// If it does not exist, return -1.
+//
+// Example 1:
+// Input: s = "leetcode"
+// Output: 0
+//
+// Example 2:
+// Input: s = "loveleetcode"
+// Output: 2
+//
+// Example 3:
+// Input: s = "aabb"
+// Output: -1
+//
+// Constraints:
+// 1 <= s.length <= 105
+// s consists of only lowercase English letters.
+
+public static class FirstUniqueCharInString
+{
+    public static int FirstUniqChar(string s)
+    {
+        Dictionary<char, int> charCount = [];
+        for (int i = 0; i < s.Length; i++)
+        {
+            if(!charCount.TryAdd(s[i], 1))
+                charCount[s[i]]++;
+        }
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (charCount[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
