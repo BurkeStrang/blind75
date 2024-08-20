0f28c3b26882496d2ce4af449ed6cce513fbf4cf
BurkeStrang <burke.strang@outlook.com>
2024-08-20 15:02:44 -0400

    new two pointers

---
+++ Blind75.Tests/TwoPointers/BackspaceStringCompare.Tests.cs
@@ -0,0 +1,17 @@
+namespace Blind75.Tests.TwoPointers;
+
+public class BackspaceStringCompareTests
+{
+    [Theory]
+    [InlineData("ab#c", "ad#c", true)]
+    [InlineData("ab##", "c#d#", true)]
+    [InlineData("a#c", "b", false)]
+    public void Example(string s, string t, bool expected)
+    {
+        // Arrange
+        // Act
+        bool actual = BackspaceStringCompare.BackspaceCompare(s,t);
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Blind75.Tests/TwoPointers/ReverseWordsInStringIII.Tests.cs
@@ -0,0 +1,16 @@
+namespace Blind75.Tests.TwoPointers;
+
+public class ReverseWordsInStringIIITests
+{
+    [Theory]
+    [InlineData("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")]
+    [InlineData("Mr Ding", "rM gniD")]
+    public void ReverseWords_ShouldReturnExpectedResult(string s, string expected)
+    {
+        // Arrange
+        // Act
+        string result = ReverseWordsInStringIII.ReverseWords(s);
+        // Assert
+        expected.Should().Be(result);
+    }
+}
