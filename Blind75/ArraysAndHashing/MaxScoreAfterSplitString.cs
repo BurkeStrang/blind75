334958ecfb65c3efd7e05bfbe2b32f395c20c990
BurkeStrang <burke.strang@outlook.com>
2024-08-21 16:04:19 -0400

    arrays

--- Blind75/ArraysAndHashing/MaxScoreAfterSplitString.cs
+++ Blind75/ArraysAndHashing/MaxScoreAfterSplitString.cs
@@ -1,8 +1,10 @@
 namespace Blind75.ArraysAndHashing;
 
-// Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings
+// Given a string s of zeros and ones, return the maximum
+// score after splitting the string into two non-empty substrings
 // (i.e. left substring and right substring).
-// The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+// The score after splitting a string is the number of zeros in the
+// left substring plus the number of ones in the right substring.
 //
 // Example 1:
 // Input: s = "011101"
