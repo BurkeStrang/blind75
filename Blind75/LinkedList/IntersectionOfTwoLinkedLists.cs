5c51f552d948bb356eb2676948339915048c9485
Burke Strang <Burke.Strang@vertigis.com>
2024-10-14 17:43:42 -0400

    wip

--- Blind75/LinkedList/IntersectionOfTwoLinkedLists.cs
+++ Blind75/LinkedList/IntersectionOfTwoLinkedLists.cs
@@ -49,12 +49,12 @@ namespace Blind75.LinkedList;
 public static class IntersectionOfTwoLinkedLists
 {
 
-// Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
     public static ListNode? GetIntersectionNode(ListNode headA, ListNode headB)
     {
         ListNode? pA = headA;
         ListNode? pB = headB;
 
+        // this checks reference equality, not value equality
         while (pA != pB)
         {
             pA = pA is null ? headB : pA.next;
