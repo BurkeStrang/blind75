4baa8f23664c72c7e122a0832f57658e4c15dcf8
Burke Strang <Burke.Strang@vertigis.com>
2025-01-24 15:39:53 -0500

    wip

--- Blind75/1dDynamicProgramming/DecodeWays.cs
+++ Blind75/1dDynamicProgramming/DecodeWays.cs
@@ -5,7 +5,9 @@ A message containing letters from A-Z can be encoded into numbers using the foll
 'A'-> "1"
 'B'-> "2"...
 'Z'-> "26"
-To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+To decode an encoded message, 
+all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways).
+For example, "11106" can be mapped into:
 
 "AAJF" with the grouping (1 1 10 6)
 "KJF" with the grouping (11 10 6)
@@ -25,7 +27,7 @@ Example 2:
 
 Input: s = "226"
 Output: 3
-Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6). 
+Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 
 */
 
