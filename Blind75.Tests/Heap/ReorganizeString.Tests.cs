6be6bf0e118224a1c8e0acfdcea2021195b79e09
Burke Strang <Burke.Strang@vertigis.com>
2025-01-31 16:02:05 -0500

    reorg

--- Blind75.Tests/Heap/ReorganizeString.Tests.cs
+++ Blind75.Tests/Heap/ReorganizeString.Tests.cs
@@ -2,8 +2,23 @@ namespace Blind75.Tests.Heap;
 
 public class ReorganizeStringTests
 {
+
     [Fact]
     public void Example1()
+    {
+        //Given
+        string s = "aaabbc";
+
+        //When
+        string res = ReorganizeString.Reorganize(s);
+
+        //Then
+        string expected = "ababac";
+        Assert.Equal(expected, res);
+    }
+
+    [Fact]
+    public void Example2()
     {
         //Given
         string s = "programming";
@@ -12,7 +27,63 @@ public class ReorganizeStringTests
         string res = ReorganizeString.Reorganize(s);
 
         //Then
-        string expected = "rmgrmgpaion";
+        string expected = "rmgmgiparon";
+        Assert.Equal(expected, res);
+    }
+
+    [Fact]
+    public void Example3()
+    {
+        //Given
+        string s = "aa";
+
+        //When
+        string res = ReorganizeString.Reorganize(s);
+
+        //Then
+        string expected = "";
+        Assert.Equal(expected, res);
+    }
+
+    [Fact]
+    public void Example4()
+    {
+        //Given
+        string s = "bbbaac";
+
+        //When
+        string res = ReorganizeString.Reorganize(s);
+
+        //Then
+        string expected = "bababc";
+        Assert.Equal(expected, res);
+    }
+
+    [Fact]
+    public void Example6()
+    {
+        //Given
+        string s = "aab";
+
+        //When
+        string res = ReorganizeString.Reorganize(s);
+
+        //Then
+        string expected = "aba";
+        Assert.Equal(expected, res);
+    }
+
+    [Fact]
+    public void Example7()
+    {
+        //Given
+        string s = "aaab";
+
+        //When
+        string res = ReorganizeString.Reorganize(s);
+
+        //Then
+        string expected = "";
         Assert.Equal(expected, res);
     }
 
