f258dca596a6a3a28c3b2ef666663a50230f593e
Burke Strang <Burke.Strang@vertigis.com>
2025-01-31 16:02:09 -0500

    wip

--- Blind75.Tests/Heap/ReorganizeString.Tests.cs
+++ Blind75.Tests/Heap/ReorganizeString.Tests.cs
@@ -6,41 +6,59 @@ public class ReorganizeStringTests
     [Fact]
     public void Example1()
     {
-        //Given
         string s = "aaabbc";
 
-        //When
         string res = ReorganizeString.Reorganize(s);
 
-        //Then
-        string expected = "ababac";
-        Assert.Equal(expected, res);
+        if (res.Length != s.Length)
+        {
+            Assert.Fail(
+                $"'{res}' is not the same length as '{s}'. Expected length: {s.Length}.");
+        }
+
+
+        for (int i = 1; i < res.Length; i++)
+        {
+            if (res[i] == res[i - 1])
+            {
+                Assert.Fail(
+                $"'{res}' has adjacent duplicates: '{res[i - 1]}' at index {i - 1} and '{res[i]}' at index {i}.");
+            }
+        }
+
     }
 
     [Fact]
     public void Example2()
     {
-        //Given
         string s = "programming";
 
-        //When
         string res = ReorganizeString.Reorganize(s);
 
-        //Then
-        string expected = "rmgmgiparon";
-        Assert.Equal(expected, res);
+
+        if (res.Length != s.Length)
+        {
+            Assert.Fail(
+                $"'{res}' is not the same length as '{s}'. Expected length: {s.Length}.");
+        }
+
+        for (int i = 1; i < res.Length; i++)
+        {
+            if (res[i] == res[i - 1])
+            {
+                Assert.Fail(
+                $"'{res}' has adjacent duplicates: '{res[i - 1]}' at index {i - 1} and '{res[i]}' at index {i}.");
+            }
+        }
     }
 
     [Fact]
     public void Example3()
     {
-        //Given
         string s = "aa";
 
-        //When
         string res = ReorganizeString.Reorganize(s);
 
-        //Then
         string expected = "";
         Assert.Equal(expected, res);
     }
@@ -48,41 +66,58 @@ public class ReorganizeStringTests
     [Fact]
     public void Example4()
     {
-        //Given
         string s = "bbbaac";
 
-        //When
         string res = ReorganizeString.Reorganize(s);
 
-        //Then
-        string expected = "bababc";
-        Assert.Equal(expected, res);
+
+        if (res.Length != s.Length)
+        {
+            Assert.Fail(
+                $"'{res}' is not the same length as '{s}'. Expected length: {s.Length}.");
+        }
+
+        for (int i = 1; i < res.Length; i++)
+        {
+            if (res[i] == res[i - 1])
+            {
+                Assert.Fail(
+                $"'{res}' has adjacent duplicates: '{res[i - 1]}' at index {i - 1} and '{res[i]}' at index {i}.");
+            }
+        }
     }
 
     [Fact]
     public void Example6()
     {
-        //Given
         string s = "aab";
 
-        //When
         string res = ReorganizeString.Reorganize(s);
 
-        //Then
-        string expected = "aba";
-        Assert.Equal(expected, res);
+
+        if (res.Length != s.Length)
+        {
+            Assert.Fail(
+                $"'{res}' is not the same length as '{s}'. Expected length: {s.Length}.");
+        }
+
+        for (int i = 1; i < res.Length; i++)
+        {
+            if (res[i] == res[i - 1])
+            {
+                Assert.Fail(
+                $"'{res}' has adjacent duplicates: '{res[i - 1]}' at index {i - 1} and '{res[i]}' at index {i}.");
+            }
+        }
     }
 
     [Fact]
     public void Example7()
     {
-        //Given
         string s = "aaab";
 
-        //When
         string res = ReorganizeString.Reorganize(s);
 
-        //Then
         string expected = "";
         Assert.Equal(expected, res);
     }
