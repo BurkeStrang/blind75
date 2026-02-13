b73d037392fe975bdd991964dec0468565cadc7d
Burke Strang <Burke.Strang@vertigis.com>
2026-02-13 17:17:22 -0500

    wip

--- Blind75.Tests/Backtracking/Permutations.Tests.cs
+++ Blind75.Tests/Backtracking/Permutations.Tests.cs
@@ -18,6 +18,23 @@ public class PermutationsTests
         Assert.Contains([3, 1, 2], result, listComparer);
         Assert.Contains([3, 2, 1], result, listComparer);
     }
+
+    [Fact]
+    public void Example1_FindPermutations()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3];
+        ListComparer<int> listComparer = new();
+        // Act
+        List<List<int>> result = Permutations.FindPermutations(nums);
+        // Assert
+        Assert.Contains([1, 2, 3], result, listComparer);
+        Assert.Contains([1, 3, 2], result, listComparer);
+        Assert.Contains([2, 1, 3], result, listComparer);
+        Assert.Contains([2, 3, 1], result, listComparer);
+        Assert.Contains([3, 1, 2], result, listComparer);
+        Assert.Contains([3, 2, 1], result, listComparer);
+    }
     [Fact]
     public void Example2()
     {
